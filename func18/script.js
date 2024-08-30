86c84799863544b46edb170485c3de5a5f09f7b9
new <gaybullayevamrayhona718@gmail.com>
2024-08-30 23:37:13 +0500

    func18

---
+++ func18/script.js
@@ -0,0 +1,24 @@
+function solve(R) {
+
+    const pi = 3.1415
+    let S = pi * (R ** 2)
+
+    return S
+
+
+}
+
+
+let R1 = 2
+let R2 = 1
+let R3 = 3
+
+let result1 = solve(R1)
+let result2 = solve(R2)
+let result3 = solve(R3)
+
+
+console.log(`birinchi doiraning yuzi ${result1}`)
+console.log(`ikkinchi doiraning yuzi ${result2}`)
+console.log(`uchinchi doiraning yuzi ${result3}`)
+
