11181fc6a683be2d4273c7c9dab6de883fcc9896
new <gaybullayevamrayhona718@gmail.com>
2024-09-01 22:20:48 +0500

    func24

---
+++ func24/script.js
@@ -0,0 +1,19 @@
+function Even(k) {
+
+    if ((k % 2) == 0) {
+        return true
+    }
+
+    else {
+        return false
+    }
+
+}
+
+let firstNum = 2
+let secondNum = 18
+let thirdNum = 5
+
+console.log(firstNum + " = " + Even(firstNum))
+console.log(secondNum + " = " + Even(secondNum))
+console.log(thirdNum + " = " + Even(thirdNum))
