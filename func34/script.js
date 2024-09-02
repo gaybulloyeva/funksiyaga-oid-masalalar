7cbd7dfbdc52ceb4d31705fc083d12bfd17bb949
new <gaybullayevamrayhona718@gmail.com>
2024-09-02 13:55:49 +0500

    func34

---
+++ func34/script.js
@@ -0,0 +1,19 @@
+function factN(n) {
+
+    let factorial = 1
+
+    for (let i = 1; i <= n; i++) {
+        factorial = factorial * i
+
+    }
+    return factorial
+}
+
+
+let first = 3
+let second = 20
+let third = 5
+
+console.log(first + " = " + factN(first))
+console.log(second + " = " + factN(second))
+console.log(third + " = " + factN(third))
