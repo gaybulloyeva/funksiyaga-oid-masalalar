d35d936e67b8a69caa0d7f9957d0d138fe4ebdbd
new <gaybullayevamrayhona718@gmail.com>
2024-09-02 14:11:41 +0500

    func37

---
+++ func37/script.js
@@ -0,0 +1,14 @@
+function power1(a, b) {
+    let result = a ** b
+    return result
+}
+
+
+let a1 = 3
+let a2 = 2
+let a3 = 5
+let b=3
+
+console.log(a1 + " ning " + b + " - darajasi " + power1(a1, b))
+console.log(a2 + " ning " + b + " - darajasi " + power1(a2, b))
+console.log(a3 + " ning " + b + " - darajasi " + power1(a3, b))
